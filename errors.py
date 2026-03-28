7ab427a3097cb9cffb9c0595872d714daaea2846
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-03-28 16:20:10 +0530

    exception handling(error handling)

--- errors.py
+++ errors.py
@@ -1,3 +1,5 @@
+'''
+
 #Logical errors
 
 number = int(input("Enter a number: "))
@@ -9,9 +11,47 @@ else:
 
 
 #Logical errors
-file = open("hello.txt", "r") #If there is no file named "hello.txt" in the same directory as this script, this will raise a FileNotFoundError.it is run time error
+file = open("hello.txt", "r") #If there is no file named "hello.txt" in the same directory as this script, this will raise a FileNotFoundError.it is run time error 
 
 content = file.read()
 print(content)
 
 file.close()
+
+'''
+
+'''
+#exception handling...
+print("Start")
+
+try:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    result = num1 / num2
+    print("The result is:", result)
+except Exception as e:
+    print(f"Error: {str(e)}")
+
+print("End")'''
+
+
+print("Start")
+
+try:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    result = num1 / num2
+    print("The result is:", result)
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+
+except ValueError:
+    print("Error: Invalid input. Please enter valid integers.")
+
+except Exception as e:
+    print(f"An unexpected error occurred: {str(e)}")
+
+finally:
+    print("Calculation completed.") #The finally block is always run if there is error or not.
+
+print("End")
